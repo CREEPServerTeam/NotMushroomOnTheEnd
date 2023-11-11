d28320ad73932052b484919537a843df3db4d3f7
qfys521 <qfys520@vip.qq.com>
2023-11-11 21:22:33 +0800

    fix: set newline symbol to `CRLF(\r\n)`

--- src/main/java/cn/qfys521/notmushroomontheend/NotMushroomOnTheEnd.java
+++ src/main/java/cn/qfys521/notmushroomontheend/NotMushroomOnTheEnd.java
@@ -1,1 +1,16 @@
-package cn.qfys521.notmushroomontheend;import org.bukkit.plugin.java.JavaPlugin;public final class NotMushroomOnTheEnd extends JavaPlugin {    public void onEnable() {        getConfig();        saveDefaultConfig();        getLogger().info("Enabling...");        getServer().getPluginManager().registerEvents(new Listeners(), this);    }    public void onDisable() {        getLogger().info("Disabling...");    }}
+package cn.qfys521.notmushroomontheend;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NotMushroomOnTheEnd extends JavaPlugin {
+    public void onEnable() {
+        getConfig();
+        saveDefaultConfig();
+        getLogger().info("Enabling...");
+        getServer().getPluginManager().registerEvents(new Listeners(), this);
+    }
+
+    public void onDisable() {
+        getLogger().info("Disabling...");
+    }
+}
--- src/main/java/cn/qfys521/notmushroomontheend/WhiteListCommands.java
+++ src/main/java/cn/qfys521/notmushroomontheend/WhiteListCommands.java
@@ -1,1 +1,37 @@
-package cn.qfys521.notmushroomontheend;import org.bukkit.Bukkit;import org.bukkit.command.Command;import org.bukkit.command.CommandExecutor;import org.bukkit.command.CommandSender;import org.bukkit.plugin.Plugin;public class WhiteListCommands implements CommandExecutor {    Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("NotMushroomOnTheEnd");    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {        if (args.length != 1) {            sender.sendMessage("Your parameter input is incorrect. You entered " + args.length                    + " parameters, but the command only accepts 1 parameter.");            return false;        }        switch (command.getName()) {            case "addMushWhitelist" -> {                this.plugin.getConfig().set(args[0], true);                sender.sendMessage("Added to the whitelist!");                this.plugin.saveConfig();                return true;            }            case "removeMushWhitelist" -> {                this.plugin.getConfig().set(args[0], false);                sender.sendMessage("Removed from the whitelist!");                this.plugin.saveConfig();                return true;            }            default -> {                return false;            }        }    }}
+package cn.qfys521.notmushroomontheend;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+
+public class WhiteListCommands implements CommandExecutor {
+    Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("NotMushroomOnTheEnd");
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length != 1) {
+            sender.sendMessage("Your parameter input is incorrect. You entered " + args.length
+                    + " parameters, but the command only accepts 1 parameter.");
+            return false;
+        }
+
+        switch (command.getName()) {
+            case "addMushWhitelist" -> {
+                this.plugin.getConfig().set(args[0], true);
+                sender.sendMessage("Added to the whitelist!");
+                this.plugin.saveConfig();
+                return true;
+            }
+            case "removeMushWhitelist" -> {
+                this.plugin.getConfig().set(args[0], false);
+                sender.sendMessage("Removed from the whitelist!");
+                this.plugin.saveConfig();
+                return true;
+            }
+            default -> {
+                return false;
+            }
+        }
+    }
+}
